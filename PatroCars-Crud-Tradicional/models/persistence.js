ada6f87649d4181110e60f7909a0f6b222c7ba7b
Jio <bleibeducuz@gmail.com>
2024-10-20 18:25:17 -0300

    alterações nas views e css correspondentes

--- PatroCars-Crud-Tradicional/models/persistence.js
+++ PatroCars-Crud-Tradicional/models/persistence.js
@@ -1,18 +1,1 @@
-//Conexão com banco de dados postgresql
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false, // Necessário se o Render ou outro serviço de hospedagem exige SSL
-        },
-    },
-});
-
-module.exports = {
-    Sequelize: Sequelize,
-    sequelize: sequelize,
-    DataTypes: Sequelize.DataTypes
-}
+.
