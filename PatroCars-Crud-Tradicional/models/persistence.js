105bafca9abf99c55267af75b17979aa93f18d72
Jio <bleibeducuz@gmail.com>
2024-10-20 15:49:08 -0300

    conectando ao banco de dados do render

--- PatroCars-Crud-Tradicional/models/persistence.js
+++ PatroCars-Crud-Tradicional/models/persistence.js
@@ -1,9 +1,14 @@
 //Conexão com banco de dados postgresql
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('montadora','postgres','123456',{
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Necessário se o Render ou outro serviço de hospedagem exige SSL
+        },
+    },
 });
 
 module.exports = {
